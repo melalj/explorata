4f7dfcc6ca54e3a9d7936bea7b5d2fa36832bd52
Simo Elalj <simo@elalj.com>
2020-03-04 21:29:51 +0100

    fixed csp

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,27 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
+const cspConfigPolicy = {
+  'default-src': "'none'",
+  'base-uri': "'self'",
+  'frame-src': "'none'",
+  'img-src ': "'self' data:;",
+  'font-src': "'self' data:;",
+  'manifest-src': "'self'",
+  'object-src': "'none'",
+  'connect-src': 'https://explorata.io/service-worker.js',
+  'script-src': ["'self'"],
+  'style-src': ["'self'"]
+};
+function addCspHtmlWebpackPlugin(config) {
+  if(process.env.NODE_ENV === 'production') {
+      config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy));
+  }
+
+  return config;
+}
 module.exports = override(
+  addCspHtmlWebpackPlugin,
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
