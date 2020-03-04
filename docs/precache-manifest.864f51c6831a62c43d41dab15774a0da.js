51da6bf98de6010ee58ee65d03819a9c0ae9be2c
Simo Elalj <simo@elalj.com>
2020-03-04 18:49:48 +0100

    added ghpages

---
+++ docs/precache-manifest.864f51c6831a62c43d41dab15774a0da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf1a84f57e0abce509da34d43068b20",
+    "url": "/ad7dc58af935b3a052cf.worker.js"
+  },
+  {
+    "revision": "2c7027ee6e8616601e24d8d4146cc42e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf2c8a3f9fe2919fb823",
+    "url": "/static/css/2.e2770e03.chunk.css"
+  },
+  {
+    "revision": "82bce215660286e366f3",
+    "url": "/static/css/main.d01cc6f5.chunk.css"
+  },
+  {
+    "revision": "cf2c8a3f9fe2919fb823",
+    "url": "/static/js/2.d6c4d66b.chunk.js"
+  },
+  {
+    "revision": "a8fa0a685d8af2934ba8ade7768fed07",
+    "url": "/static/js/2.d6c4d66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82bce215660286e366f3",
+    "url": "/static/js/main.4d91c901.chunk.js"
+  },
+  {
+    "revision": "582e4eaa29408d13e34e",
+    "url": "/static/js/runtime-main.fd8f6328.js"
+  },
+  {
+    "revision": "bfea5b70a6a8fc2e995e0840e4ff6e00",
+    "url": "/static/media/instructionsDeselect.bfea5b70.png"
+  },
+  {
+    "revision": "e356adc2c13aa37402c965d12605dcd5",
+    "url": "/static/media/instructionsFilecreated.e356adc2.png"
+  },
+  {
+    "revision": "cc5d8bdd98d3481dfde98aa6f09b771f",
+    "url": "/static/media/instructionsRequest.cc5d8bdd.png"
+  },
+  {
+    "revision": "ef9dbcf108f0ad434c5bb9f32e8bb23e",
+    "url": "/static/media/instructionsSelect.ef9dbcf1.png"
+  }
+]);
