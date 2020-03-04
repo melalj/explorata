abd4c4f33854fd9b725e69319d8fb6eac652b06e
Simo Elalj <simo@elalj.com>
2020-03-04 22:06:02 +0100

    More text on the homepage

--- src/components/Dropfiles.jsx
+++ src/components/Dropfiles.jsx
@@ -14,12 +14,6 @@ import { screens } from '../constants';
 import * as Actions from '../state/actions';
 import { getWorker } from '../utils/worker';
 
-
-function handleRepoClick(e) {
-  e.stopPropagation();
-  if (window) window.open('https://www.github.com/melalj/explorata');
-}
-
 class Dropfiles extends React.Component {
   handleInstructionsClick(e) {
     const { goInstructions } = this.props;
@@ -76,50 +70,61 @@ class Dropfiles extends React.Component {
 
     if (isLoading) return null;
     return (
-      <div className="page">
-        <Dropzone
-          ref={this.dropzoneRef}
-          onDrop={f => this.dropHandler(f)}
-          accept=".json"
-          multiple
-        >
-          {({ isDragActive, getRootProps, getInputProps }) => (
-            <div
-              {...getRootProps()}
-              className={classNames('dropzone', {
-                'is-active': isDragActive
-              })}
-            >
-              <input
-                {...getInputProps()}
-                directory=""
-                webkitdirectory=""
-                nwdirectory=""
-              />
-              <p>
-                {isDragActive
-                  ? 'Yes! here...'
-                  : 'Drag and drop your “messages” folder here...'}
-              </p>
-              <div>
-                <Button type="primary" size="large">
-                  Choose from computer...
-                </Button>
-                <Button size="large" onClick={(e) => this.handleInstructionsClick(e)}>
-                  Instructions to get your dataset
-                </Button>
+      <div>
+        <div className="page">
+          <div className="logo">Explorata<span className="dot">.</span></div>
+          <div className="logo-description">
+            <strong>Visualize crispy data from your Facebook Messenger export</strong><br />
+            🏆 Most messaged friends? 🔥 Best streaks? 🗓Most active year?<br />
+            👻 Ghosters or 🗣 Talkers? 😜 Most sent/received Emoji? ...
+          </div>
+          <Dropzone
+            ref={this.dropzoneRef}
+            onDrop={f => this.dropHandler(f)}
+            accept=".json"
+            multiple
+          >
+            {({ isDragActive, getRootProps, getInputProps }) => (
+              <div
+                {...getRootProps()}
+                className={classNames('dropzone', {
+                  'is-active': isDragActive
+                })}
+              >
+                <input
+                  {...getInputProps()}
+                  directory=""
+                  webkitdirectory=""
+                  nwdirectory=""
+                />
+                <p>
+                  {isDragActive
+                    ? 'Yes! here...'
+                    : 'Drag and drop your “messages” folder here...'}
+                </p>
+                <div>
+                  <Button type="primary" size="large">
+                    Choose from computer...
+                  </Button>
+                  <Button size="large" onClick={(e) => this.handleInstructionsClick(e)}>
+                    Instructions to get your dataset
+                  </Button>
+                </div>
               </div>
-              <div className="disclaimer">
-                Everything is analyzed locally right on your computer.
-                <br />
-                Explorata do not store or send any information to any external server.
-                <br />
-                The source code is available on
-                <a onClick={e => handleRepoClick(e)}> Github</a>
-              </div>
-            </div>
-          )}
-        </Dropzone>
+            )}
+          </Dropzone>
+          <div className="disclaimer">
+            <strong>Privacy is by design with Explorata</strong><br />
+            - You will certainly get an alert “Upload X files to this site?” – None of your files or result are uploaded to a server. All computations are happening right on your browser.<br />
+            - You can totally use this tool offline (Give it a shot)<br />
+            - The source code of this project is open source on <a href="https://github.com/melalj/explorata" rel="noopener noreferrer" target="_blank">GitHub</a>.<br />
+            - This website is static and is deployed on <a href="https://github.com/melalj/explorata/tree/gh-pages" rel="noopener noreferrer" target="_blank">GitHub Pages</a>.<br />
+            - You can <a href="https://github.com/melalj/explorata" rel="noopener noreferrer" target="_blank">read more</a> about our motivation and privacy mesures we took for Explorata. If you want to contribute to this project, you can <a href="https://github.com/melalj/explorata/issues" rel="noopener noreferrer" target="_blank">report an issue</a> or open a <a href="https://github.com/melalj/explorata" rel="noopener noreferrer" target="_blank">pull request</a>.
+          </div>
+        </div>
+        <div className="footer">
+          Made with <a href="https://www.foodandwine.com/drinks/everything-you-need-know-about-pastis" rel="noopener noreferrer" target="_blank">Pastis️</a> by <a href="https://tonoid.com" rel="noopener noreferrer" target="_blank">Simo Elalj</a> & <a href="https://ulysse.xyz" rel="noopener noreferrer" target="_blank">Ulysse Sabbag</a> in Aix-en-Provence with the <a href="https://mozza.io" rel="noopener noreferrer" target="_blank">Mozza</a> crew
+        </div>
       </div>
     );
   }
